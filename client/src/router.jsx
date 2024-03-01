0f578c957465a1862789f5b97ac8fa491a1ce86f
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2024-03-01 11:17:38 +0000

    feat - protected routes

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,6 +1,5 @@
-import { Suspense, lazy } from "react";
+// import { Suspense, lazy } from "react";
 import { Navigate } from "react-router-dom";
-import { RouteObject } from "react-router";
 
 //Import layouts
 import BaseLayout from "./components/Layouts/BaseLayout";
@@ -9,9 +8,37 @@ import AppbarLayout from "./components/Layouts/AppbarLayout";
 //Import pages
 import { Explore } from "./pages/Explore";
 
+//TODO: Implement Authentication
+// Fake authentication function
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+    //Todo: If not authenticated, redirect to Sign in/Sign up
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+// Protected Route Component
+const ProtectedRoute = ({ element, ...rest }) => {
+  return fakeAuth.isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/auth/signin" replace />
+  );
+};
+
 const routes = [
   {
-    path: "/auth",
+    path: "",
+    element: <Navigate to="explore" replace />,
+  },
+  {
+    path: "auth",
     element: <BaseLayout />,
     // children: [
     //   {
@@ -21,7 +48,7 @@ const routes = [
     // ],
   },
   {
-    path: "/explore",
+    path: "explore",
     element: <AppbarLayout />,
     children: [
       {
@@ -29,5 +56,15 @@ const routes = [
         element: <Explore />,
       },
     ],
+  },
+  {
+    path: "plans",
+    element: <ProtectedRoute element={<BaseLayout />} />,
+    // children: [
+    //   {
+    //     path: "",
+    //     element: <MyPlans/>
+    //   },
+    // ],
   },
 ];
