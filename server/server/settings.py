4b2fdc9ccb79c0c9182bba271fb6be66716eb239
talehafandi <alhuseynov.taleh@gmail.com>
2024-03-03 12:48:27 +0000

    refactor-s: migrate from postgres to sqlite3

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASS'),
+#         'HOST': config('DB_HOST'),  
+#         'PORT': config('DB_PORT'),      
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),  
-        'PORT': config('DB_PORT'),      
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  
     }
 }
 
