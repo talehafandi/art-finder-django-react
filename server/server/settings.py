42f22203bd48acafb6614212324e077b2e375af6
talehafandi <alhuseynov.taleh@gmail.com>
2025-01-18 21:24:52 +0400

    fix: rm django secret

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9i)2@25_u@pxpz(a5*t++$s8n#7rsxtl6q+b=3atbmius%0sdo'
+SECRET_KEY = config(DJANGO_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
